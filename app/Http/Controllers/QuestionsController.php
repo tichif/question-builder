88cdae12b6a895a7e9c753fd3c18a130c062165d
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-02-27 21:55:42 -0500

    Validating and saving the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 
 class QuestionsController extends Controller
@@ -38,9 +39,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title','body'));
+
+        return redirect()->action('QuestionsController@index')->with('success','Your question has been submitted');
     }
 
     /**
--- resources/views/pages/questions/create.blade.php
+++ resources/views/pages/questions/create.blade.php
@@ -20,7 +20,7 @@
                     {{ csrf_field() }}
                     <div class="form-group">
                       <label for="question-title">Question Title</label>
-                      <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}">
+                      <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}" value="{{ old('title') }}">
                       @if ($errors->has('title'))
                         <div class="invalid-feedback">
                           <strong>{{ $errors->first('title') }}</strong>
@@ -29,7 +29,7 @@
                     </div>
                     <div class="form-group">
                       <label for="question-body">Explain your question</label>
-                      <textarea name="body" id="question-body" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" rows="10"></textarea>
+                      <textarea name="body" id="question-body" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" rows="10" value="{{ old('body') }}"></textarea>
                       @if ($errors->has('body'))
                         <div class="invalid-feedback">
                           <strong>{{ $errors->first('body') }}</strong>
