79346ed2f393a6b9d9304b3dad9db19fd22b302c
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-02-27 19:31:36 -0500

    Adding votes, answers and views counters

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,7 +14,9 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
+        $questions = Question::with('user')
+                            ->latest()
+                            ->paginate(5);
 
         return view('pages.questions.index', compact('questions'));
     }
