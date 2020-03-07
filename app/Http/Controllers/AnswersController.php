1673915813545c0afe877987c21ccab29f40e388
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-03-06 21:32:44 -0500

    Updating answer for specific question

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -25,7 +25,7 @@ class AnswersController extends Controller
 
         $question->answers()->create(['body' => $request->body, 'user_id' => \Auth::id()]);
 
-        return back()->with('success', 'Your answer has been submitted') ;
+        return back()->with('success', 'Your answer has been submitted successfully !!!') ;
     }
 
     
@@ -33,24 +33,34 @@ class AnswersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Question  $question
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question,Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+
+        return view('pages.answers.edit', compact('question', 'answer'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Question  $question
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+
+        $answer->update($request->validate([
+            'body' => 'required'
+        ]));
+
+        return redirect()->route('questions.show', $question->slug)->with('success','Your answer has been updated successfully !!!') ;
     }
 
     /**
