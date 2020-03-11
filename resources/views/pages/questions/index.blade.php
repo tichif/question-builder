4ed4c7a9c07839472cb4cb49b1cc1b914ab338b2
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-03-11 14:29:15 -0400

    Tidy the views

--- resources/views/pages/questions/index.blade.php
+++ resources/views/pages/questions/index.blade.php
@@ -18,55 +18,18 @@
                 <div class="card-body">
                   @include('layouts._messages')
                   @forelse ($questions as $question)
-                      <div class="media">
-                        <div class="d-flex flex-column counters">
-                          <div class="vote">
-                            <strong>{{ $question->votes_counts }}</strong> {{ str_plural('vote', $question->votes_counts) }}
-                          </div>
-                          <div class="status {{ $question->status }}">
-                            <strong>{{ $question->answers_count }}</strong> {{ str_plural('answer', $question->answers_count) }}
-                          </div>
-                          <div class="view">
-                           {{ $question->views ." ". str_plural('view', $question->views) }}
-                          </div>
-                        </div>
-                        <div class="media-body">
-                          <div class="d-flex align-items-center">
-                            <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                            <div class="ml-auto">
-                              @can ('update', $question)
-                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                              @endcan
-                              @can ('delete', $question)
-                                <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="POST">
-                                  {{ method_field('DELETE') }}
-                                  @csrf
-                                  <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure ?')">Delete</button>
-                                </form>
-                              @endcan
-                            </div>
-                          </div>
-                          <p class="lead">
-                            Asked by <a href="{{ $question->user->url }}">
-                              {{ $question->user->name }}
-                            </a>,
-                            <small class="text-muted">{{ $question->created_date }}</small>
-                          </p>
-                          <div class="excerpt">
-                            {{ $question->excerpt }}
-                          </div> 
-                        </div>
-                      </div>
-                      <hr>
+
+                      @include('pages.questions._excerpt')
+                      
                       @empty
                           <div class="alert alert-warning">
                             <strong>Sorry !!!</strong> There are no questions available !!!
                           </div>
                   @endforelse
 
-                  <div>
+                  
                     {{ $questions->links() }}
-                  </div>
+                  
                 </div>
             </div>
         </div>
